86c4f71bc017a017d256a564e5d022ed7a2d9506
Benji Olsen <benjaminalanolsen@gmail.com>
2018-10-14 11:24:42 +0800

    Wrote the basis for some of the needed operations to be performed on the data

--- operations.c
+++ operations.c
@@ -1,1 +1,45 @@
 #include "operations.h"
+
+int round(double value)
+{/* I have seen this done in less code, but i understand my way better */
+    int num;
+    if(value >= 0)
+    {/* If the number is positive, adding .5 and truncing will put it to the
+        nearest whole value */
+        num = (int)(value + 0.5);
+    }
+    else
+    {/* ditto but negative and -.5 */
+        num = (int)(value - 0.5);
+    }
+    return num;
+}
+
+void radians(double* degrees)
+{/* RAD = DEG * PI/180 ezpz*/
+    degrees = ( degrees * (PI/180) );
+}
+
+Plotfunc plotter(void *plotData)
+{
+    if(((char*)plotdata == plotdata)&&(plotData != NULL))
+    {
+        printf("%c",(char*)plotData);
+    }
+}
+
+Commands* extract(LinkedList list)
+{
+    length = getLength(list);
+    int ii, length = getLength(list);
+    for(ii=0;ii<length;ii++)
+    {
+
+    }
+}
+
+int test(LinkedList* list)
+{
+    int correct = FALSE;
+    return correct;
+}
--- operations.h
+++ operations.h
@@ -1,10 +1,24 @@
 #ifndef OPERATIONS_H
+/* Header Guard */
 #define OPERATIONS_H
 
+/* Why the FLIP is this not in the FLIPPING math library */
+/* C89 is where its at bois */
+/* Defines the known mathematical constant, pi, to 4 decimal places */
+#define PI 3.1415
+
+#include <math.h>
+
+/* Defines the struct to hold command and its value, along with the pattern
+   that it would correspond to */
 typdef struct
 {
     char* operation;
+    char pattern;
     void* value;
 }Commands;
+
+/* Will run through the commands and ensure each one entered was valid */
+int test(LinkedList* list);
 
 #endif
