6d14a65f58a0e3b1fc4a3889c13c7eae7732f292
Benji Olsen <benjaminalanolsen@gmail.com>
2018-10-15 10:02:10 +0800

    Added upperCase and some WIP functions

--- operations.c
+++ operations.c
@@ -30,16 +30,58 @@ Plotfunc plotter(void *plotData)
 
 Commands* extract(LinkedList list)
 {
-    length = getLength(list);
     int ii, length = getLength(list);
-    for(ii=0;ii<length;ii++)
-    {
+    char* number;
+    char *string, *pattern;
+    Commands* commands = NULL;
+    ListNode cur = list->head;
 
+    commands = (Commands*)malloc(length*sizeof(Commands));
+    while(sscanf(list->data,"%s %s",&string,&number)!=NULL)
+    {
+        upperCase(string);
+        if(strcmp(string,"PATTERN"))
+        {
+            strcpy(number,pattern)
+        }
+        else
+        {
+            strcpy(commands[ii]->Operation,string);
+        }
+
+        ii++;
     }
 }
 
-int test(LinkedList* list)
+int test(Commands* commands)
 {
+    char move[] = "MOVE";
+    char rotate[] = "ROTATE";
+    char draw[] = "DRAW";
+    char fg[] = "FG";
+    char bg[] = "BG";
+    char pattern[] = "PATTERN";
     int correct = FALSE;
+
+    do
+    {
+
+    }while();
+
     return correct;
+}
+
+void upperCase(char* string)
+{
+    int ii = 0;
+    int length = 0;
+
+    length = strlen(string);
+    for(ii=0;ii<length;ii++)
+    {
+        if(string[ii] <= 122 && string[ii] >= 97)
+        {/* If it is a lowercase letter, make it uppercase */
+            string[ii] = string[ii] - 32;
+        }
+    }
 }
