fecf945184860d7dcbd76eb26b9aadf0c44ed573
Benji Olsen <benjaminalanolsen@gmail.com>
2018-10-17 14:38:19 +0800

    Added the ability to print to a log file, or use simple colours depending on the specifications

--- operations.c
+++ operations.c
@@ -32,11 +32,15 @@ int execute(LinkedList* list)
 {
     ListNode* cur = NULL;
     Position* pos = (Position*)malloc(sizeof(Position));
+    LinkedList* logList = NULL;
     char pattern = '+';
+    char logString[BUFFER_SIZE],*value,*command;
     double dist = 0.0;
     int valid = TRUE;
     double angle = 0.0;
     double newAngle = 0.0;
+
+    logList = makeEmpty();
 
     pos->x1 = 0.0;
     pos->x2 = 0.0;
@@ -49,12 +53,22 @@ int execute(LinkedList* list)
     setBgColour(0);
 
     do
-    {
+    {/* Runs through the commands executing their values */
         if(strcmp(cur->command,"MOVE")==0)
         {
             dist = atof(cur->value)-1.0;
             distance(pos,angle,1.0);
             distance(pos,angle,dist);
+            sprintf(logString,"%4s (%07.3f,%07.3f)-(%07.3f,%07.3f)\n",
+                                                            cur->command,
+                                                            pos->x1,pos->y1,
+                                                            pos->x2,pos->y2);
+            value = stringDupe(" ");
+            command = stringDupe(logString);
+            #ifdef DEBUG
+            fprintf(stderr,"%s",logString);
+            #endif
+            insertFirst(logList,command,value);
         }
         else if(strcmp(cur->command,"DRAW")==0)
         {
@@ -63,6 +77,16 @@ int execute(LinkedList* list)
             distance(pos,angle,dist);
             line(round(pos->x1),round(pos->y1),round(pos->x2),
                  round(pos->y2),&plotter,&pattern);
+                 sprintf(logString,"%4s (%07.3f,%07.3f)-(%07.3f,%07.3f)\n",
+                                                            cur->command,
+                                                            pos->x1,pos->y1,
+                                                            pos->x2,pos->y2);
+                 value = stringDupe(" ");
+                 command = stringDupe(logString);
+                 #ifdef DEBUG
+                 fprintf(stderr,"%s",logString);
+                 #endif
+                 insertFirst(logList,command,value);
         }
         else if(strcmp(cur->command,"ROTATE")==0)
         {
@@ -70,6 +94,8 @@ int execute(LinkedList* list)
             radians(&newAngle);
             rotate(&angle,newAngle);
         }
+
+        #ifndef SIMPLE
         else if(strcmp(cur->command,"FG")==0)
         {
             if(atoi(cur->value)>=0)
@@ -84,16 +110,20 @@ int execute(LinkedList* list)
                 setBgColour(round(atoi(cur->value)));
             }
         }
+        #endif
+
         else if(strcmp(cur->command,"PATTERN")==0)
         {
-            if(cur->value[0] == ' ')
-            {
-                valid = FALSE;
-            }
             pattern = cur->value[0];
         }
+
         cur = cur->next;
     }while(cur!=NULL&&valid == TRUE);
+    if(commandLog(logList)==TRUE)
+    {
+        fprintf(stderr,"There was an error with the logfile!\n");
+    }
+    freeList(logList);
     penDown();
     free(pos);
     return valid;
@@ -132,7 +162,34 @@ int test(LinkedList* list)
            strcmp(cur->command,bg)==0 ||
            strcmp(cur->command,pattern)==0)
         {
-
+            if(strcmp(cur->command,rotate)==0)
+            {
+                if(atof(cur->value)>360.0||atof(cur->value)<0.0)
+                {
+                    correct = FALSE;
+                }
+            }
+            if(strcmp(cur->command,fg)==0)
+            {
+                if(atoi(cur->value)<0||atoi(cur->value)>15)
+                {
+                    correct = FALSE;
+                }
+            }
+            if(strcmp(cur->command,bg)==0)
+            {
+                if(atoi(cur->value)<0||atoi(cur->value)>7)
+                {
+                    correct = FALSE;
+                }
+            }
+            if(strcmp(cur->command,pattern)==0)
+            {
+                if(strcmp(cur->value," ")==0)
+                {
+                    correct = FALSE;
+                }
+            }
         }
         else
         {
